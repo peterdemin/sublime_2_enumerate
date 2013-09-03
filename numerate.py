e4297fec88bb32de4d18184c8ce321b95beab8a3
peterdemin <poslano@gmail.com>
2013-09-03 11:59:25 +0400

    Critical bug fix
    enumerate class name was conflicting with enumerate function I use.

--- numerate.py
+++ numerate.py
@@ -1,7 +1,7 @@
 import sublime_plugin
 
 
-class enumerate(sublime_plugin.TextCommand):
+class numerate(sublime_plugin.TextCommand):
     def run(self, edit):
         for i, region in enumerate(self.view.sel()):
             self.view.insert(edit, region.begin(), str(i + 1))
